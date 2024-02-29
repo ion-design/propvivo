d94c442eb30649ec4dc429eb69efa41d270dc152
Samraaj Bath <5092624+Samraaj@users.noreply.github.com>
2024-02-28 19:28:46 -0800

    sheet 2

--- src/components/ion/PropVivoTopBar.tsx
+++ src/components/ion/PropVivoTopBar.tsx
@@ -15,20 +15,26 @@ import Button from "@/components/ion/Button";
 import Avatar from "@/components/ion/Avatar";
 import clsx from "clsx";
 import Sheet from "../Sheet";
-import SheetContentForm from "./SheetContentForm";
+import SheetContentForm from "./sheets/SheetContentForm";
 import UpdateDetailsConfirmationModal from "./modals/UpdateDetailsConfirmationModal";
 import SuccessConfirmationModal from "./modals/SuccessConfirmationModal";
 import CannotDeleteModal from "./modals/CannotDeleteModal";
+import ChartOfAccountsSheet from "./sheets/ChartOfAccountsSheet";
 type PropVivoTopBarProps = {
    className?: string;
 };
 
 function PropVivoTopBar({ className = "" }: PropVivoTopBarProps) {
    const [inputValue, setInputValue] = useState("");
-   const [open, setOpen] = useState(false);
+   const [chartSheetOpen, setChartSheetOpen] = useState(false);
+   const [editSheetOpen, setEditSheetOpen] = useState(false);
 
    function createNewClickHandler(e: MouseEvent<HTMLButtonElement>) {
-      setOpen(true);
+      setChartSheetOpen(true);
+   }
+   function editClickHandler(e: MouseEvent<HTMLButtonElement>) {
+      setChartSheetOpen(false);
+      setEditSheetOpen(true);
    }
    function buttonOnClickHandler(e: MouseEvent<HTMLButtonElement>) {
       alert("buttonOnClickHandler fired");
@@ -42,12 +48,20 @@ function PropVivoTopBar({ className = "" }: PropVivoTopBarProps) {
       >
          <div className="flex items-start gap-5">
             <Sheet
-               onOpenChange={setOpen}
-               open={open}
+               onOpenChange={setEditSheetOpen}
+               open={editSheetOpen}
                title="Edit Chart of Acounts Header/ Sub-Header"
                footer={<Button>Save</Button>}
             >
                <SheetContentForm />
+            </Sheet>
+            <Sheet
+               onOpenChange={setChartSheetOpen}
+               open={chartSheetOpen}
+               title="Chart of Accounts Header/ Sub-Header"
+               footer={<Button onClick={editClickHandler} className="w-full">Edit</Button>}
+            >
+               <ChartOfAccountsSheet />
             </Sheet>
             <Input
                iconTrailing={<MagnifyingGlass size={16} weight={"regular"} />}
